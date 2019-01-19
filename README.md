68df2f81a78954f3b89beb0cd0b717ba616d4445
Ravik <ravik@in-Ravik.local>
2019-01-19 12:16:30 +0530

    Ravi | Add mock for articles

---
+++ README.md
@@ -0,0 +1,11 @@
+# mockServer
+
+This provides a mock api response for all the API's used in development of [BlogUserInterface](https://github.com/ILearntToday/BlogUserInterface).
+
+Usage:
+- npm install
+- node index.js
+
+Mocked API list
+
+- /api/v1/articles/:parent_id
---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+const port = 8080;
+
+app.get('/api/v1/articles/:parent_id', (request, response) => {
+    response.setHeader('Content-Type', 'application/json');
+    fs.readFile('mockResponses/articles.json', (err, data) => {
+        if (err) throw err;
+        let body = JSON.parse(data);
+
+        console.log("===============================================================");
+        console.log("Requested article id: ", request.params.parent_id);
+        console.log("Response body: ", data.toString());
+        console.log("===============================================================");
+
+        response.send(body);
+    });
+});
+
+app.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err)
+    }
+
+    console.log(`server is listening on ${port}`)
+});
